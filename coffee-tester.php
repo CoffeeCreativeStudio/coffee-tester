b2112070ff66aa95c95b2b8ce455cbd4f0bf60a2
Frits Steyn <fritkid@gmail.com>
2016-07-28 14:41:34 +0200

    Version #
    changed version number from 1.0.0 to 1.0.1

--- coffee-tester.php
+++ coffee-tester.php
@@ -3,7 +3,7 @@
 Plugin Name: Coffee Tester
 Plugin URI: http://www.coffeecreativestudio.co.za
 Description: Testing github shizzle since 2016
-Version: 1.0.0
+Version: 1.0.1
 Text Domain: coffee-tester
 Author: Frits Steyn
 Author URI: http://frits.co.za
