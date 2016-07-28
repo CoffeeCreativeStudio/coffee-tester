5fa049ecf5195cb618e8c9cc0d1e221be4474397
Frits Steyn <fritkid@gmail.com>
2016-07-28 14:29:37 +0200

    start

---
+++ coffee-tester.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Coffee Tester
+Plugin URI: http://www.coffeecreativestudio.co.za
+Description: Testing github shizzle since 2016
+Version: 1.0.0
+Text Domain: coffee-tester
+Author: Frits Steyn
+Author URI: http://frits.co.za
+*/
